7655c8c4063e1f8b7b167406883733dcb32b40a9
MicRofaRatOV <ps.fx@ya.ru>
2023-03-27 11:18:08 +0300

    123

--- telegram/src/messages.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-sys.path.insert(1, './localization')
-
-from ru_RU import *
-# from en_US import *  # you can help me xD
--- telegram/src/random_generator.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-def generate_md5_str():
-    return "%032x" % random.getrandbits(128)
